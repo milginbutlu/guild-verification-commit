c329b56accec6eea0588e0dd4605a042905293dc
milginbutlu <166455351+milginbutlu@users.noreply.github.com>
2026-03-11 15:32:06 +0300

    Fix memory leak in event handlers

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:31:57.900Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-11T12:32:06.827Z
+Commit: Fix memory leak in event handlers
