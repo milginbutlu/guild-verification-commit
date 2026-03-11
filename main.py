341afe6569bd4faaf9fe73d92e05927f4cf37bd2
milginbutlu <166455351+milginbutlu@users.noreply.github.com>
2026-03-11 15:22:43 +0300

    Add export functionality for reports

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:22:39.676Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-11T12:22:44.579Z
+Commit: Add export functionality for reports
