3f7ede62c8994652f05f613a87ea069b070072ad
milginbutlu <166455351+milginbutlu@users.noreply.github.com>
2026-03-11 15:22:40 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:22:25.528Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-11T12:22:41.647Z
+Commit: Resolve timezone issues in date handling
