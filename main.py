bfdc85c8356a0e88faf71eafd8adac686b73a204
milginbutlu <166455351+milginbutlu@users.noreply.github.com>
2026-03-11 15:32:32 +0300

    Fix CSS styling bug on mobile devices

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:32:17.398Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-11T12:32:33.576Z
+Commit: Fix CSS styling bug on mobile devices
