f5d5ba8cc2c48e39161cfbaa40144365aa538d28
milginbutlu <166455351+milginbutlu@users.noreply.github.com>
2026-03-11 15:31:49 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:31:35.302Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-11T12:31:50.198Z
+Commit: Fix broken links in documentation
