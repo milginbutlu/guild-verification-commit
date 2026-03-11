21c399a3c9a474c3713e8adc2a9fd16ccac1e1d3
milginbutlu <166455351+milginbutlu@users.noreply.github.com>
2026-03-11 15:32:07 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:31:50.198Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-11T12:32:07.832Z
+Commit: Fix memory leak in event handlers
